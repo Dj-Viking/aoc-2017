3eed84ceb063b577bd40fd0fcc74252bf424c081
dj-viking <anders.swedishviking@gmail.com>
2024-06-12 18:03:06 -0400

    start day2

---
+++ Day2/Day2.ps1
@@ -0,0 +1,11 @@
+        param(
+            [string]
+            $inputtype = "sample"
+        )
+
+        [System.Collections.ArrayList]$lines = @();
+
+        Get-Content -Path $(if ($inputtype -ne "sample") { "input.txt" } else { "sample.txt" }) 
+        | ForEach-Object {
+            $lines.Add($_) | Out-Null
+        };
