ab0579564e74c779f6bbbf1b08db5bdec2253f06
dj-viking <anders.swedishviking@gmail.com>
2024-06-12 18:50:47 -0400

    fix startup code, solve day2 part1, debug day 2 launch settings

--- Day2/Day2.ps1
+++ Day2/Day2.ps1
@@ -5,7 +5,37 @@ param(
 
 [System.Collections.ArrayList]$lines = @();
 
-Get-Content -Path $(if ($inputtype -ne "sample") { "input.txt" } else { "sample.txt" }) `
-| ForEach-Object {
-    $lines.Add($_) | Out-Null
-};
+[string]$content = Get-Content -Raw -Path $(if ($inputtype -notmatch "sample") { "$($PSScriptRoot)\input.txt" } else { "$($PSScriptRoot)\sample.txt" });
+
+$content | ForEach-Object {
+    $line = [string]$_;
+    $splitrawline = $line.Split([System.Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries);
+    $splitrawline | ForEach-Object {
+        $lines.Add($_) | Out-Null;
+    }
+}
+
+function part1 {
+    $answer1 = 0;
+
+    foreach ($l in $lines) {
+        [string]$_l = $l;
+
+        [System.Collections.ArrayList]$rownums = @(); 
+        
+        $_l.Split("", [System.StringSplitOptions]::RemoveEmptyEntries) `
+        | ForEach-Object {
+            $rownums.Add([int]([string]$_)) | Out-Null;
+        };
+
+        $rownums.Sort();
+
+        $answer1 += $rownums[-1] - $rownums[0];
+    }
+
+    Write-Host "answer 1: <$answer1>";
+}
+function part2 {}
+
+part1;
+part2;
