da5677f0b814ceb1e8866ba0354e9ef3b329e4ab
dj-viking <anders.swedishviking@gmail.com>
2024-06-14 21:03:14 -0400

    solve part1 day3 with help from kezzryn because I don't know manhattan distance, add another launch profile for dotnet-script

--- Day3/Day3.ps1
+++ Day3/Day3.ps1
@@ -3,9 +3,27 @@ param(
     $inputtype = "sample"
 )
 
-[System.Collections.ArrayList]$lines = @();
+[int]$input1 = 368078;
 
-Get-Content -Path $(if ($inputtype -notmatch "sample") { "$($PSScriptRoot)\input.txt" } else { "$($PSScriptRoot)\$inputtype.txt" }) `
-| ForEach-Object {
-    $lines.Add($_) | Out-Null
-};
+# end point of the grid trace back to start (0, 0) how many steps?
+
+
+function part1 {
+
+    # powershell int casting will round a float up so don't add one here
+    [int]$upperbase = [int]([System.Math]::Sqrt($input1));
+    [int]$upperlimit = [int]([System.Math]::Pow($upperbase, 2));
+
+    # dont int cast here....because powershell?????
+    [int]$segment = ([System.Math]::Sqrt($input1) / 2);
+    
+    # subtract 1 from upper base because ?????? no idea it works though
+    [int]$answer = (($upperbase - 1) / 2) + ($upperlimit - $input1 + $segment) % $segment;
+
+    #374 too high
+    #370 too low
+    #371 according to my solution example from kezzryn
+    Write-Host "part1: $answer"
+}
+
+part1;
